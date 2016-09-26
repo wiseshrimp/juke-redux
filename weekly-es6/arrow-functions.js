aa39e63060f294e0316f68c9082287f32ce40343
Ben Cohen <ben.cohen@fullstackacademy.com>
2016-09-26 11:37:31 -0400

    updated arrow function code

--- weekly-es6/arrow-functions.js
+++ weekly-es6/arrow-functions.js
@@ -27,6 +27,34 @@ var traditional = {
   siblings: ["Lauren", "Ben"],
   printFriends: function () {
     this.siblings.forEach(function(sibling) {
+      // this.name will not refer to "Ben" because 
+      // this is rebound in the new function passed to forEach
+      console.log(this.name, "<3", sibling)
+    });
+  }
+}
+
+var oldWay = {
+  name: "Ben",
+  siblings: ["Lauren", "Ben"],
+  printFriends: function () {
+    var that = this;  
+    this.siblings.forEach(function(sibling) {
+      // this will work because we bound this to that
+      // when it had the relevant value
+      console.log(that.name, "<3", sibling)
+    });
+  }
+}
+
+var es6way = {
+  name: "Ben",
+  siblings: ["Lauren", "Ben"],
+  printFriends: function () {
+    this.siblings.forEach(sibling => {
+      // this will work because arrow functions
+      // do not rebind this, so it still has the context
+      // of its parent function
       console.log(this.name, "<3", sibling)
     });
   }
